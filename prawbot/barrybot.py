6409002f14cf113792c54d56111a43f74da0305a
pusherosu <thedosius_dreamtwister@hotmail.com>
2019-05-16 11:09:27 -0300

    Update barrybot.py

--- prawbot/barrybot.py
+++ prawbot/barrybot.py
@@ -2,6 +2,8 @@
 import random
 import praw
 import time
+import logging
+import sys
 
 # Definitions
 RATE_LIMIT = 660
@@ -10,33 +12,40 @@ reddit = praw.Reddit('bot1')
 subreddit = reddit.subreddit("hooksplays")
 start_time = time.time()
 
+# Basic logging configuration
+logging.basicConfig(level=logging.INFO, 
+			filename='barry.log', 
+			filemode='a', 
+			format='%(asctime)s %(levelname)s : %(message)s')
+
+# Not running python 3? Pull the ripcord!
+if sys.version_info[0] < 3:
+	raise ValueError ("This script requires Python 3")
+	logging.critical("This script requires Python 3")
+	sys.exit()
+
 with open ('barryisms.txt') as f:
 	barryisms = [line for line in f]
+
+#print(reddit.config.username)	
 	
 def start_stream(USERNAME):
 	for comment in subreddit.stream.comments():
 		normalized_comment = comment.body.lower()
 		if comment.created_utc < start_time:
 			continue
-		if "barry" in normalized_comment:
-			print("{}: {} has summoned the Barry!".format(comment.created_utc,comment.author.name))
+		if "barry" in normalized_comment and comment.author.name != reddit.config.username:
+			logging.info("{} has summoned the Barry!".format(comment.author.name))
 			reply_text = (random.choice(barryisms))
 			comment.reply(reply_text)
 
 if __name__ == "__main__":
-	print("{}: Starting the bot...\n".format(time.ctime()))
+	logging.info("Starting the bot...")
 	while True:
 		try:
 			start_stream(USERNAME)
 		except (praw.exceptions.PRAWException , Exception) as e:
-			print("{}: Exception: {}. Starting cooldown timer.\n".format(format(time.ctime(),e)))
+			logging.error("Exception: {}. Starting cooldown timer.".format(e))
 			time.sleep(RATE_LIMIT)
 			start_time = time.time()
-			print("{}: Restarting stream...\n".format(time.ctime()))
-			continue
-#		except Exception as er:
-#			print("{}: Exception: {}. Starting cooldown timer.\n".format(format(time.ctime(),er)))
-#			time.sleep(RATE_LIMIT)
-#			start_time = time.time()
-#			print("{}: Restarting stream...\n".format(time.ctime()))
-#			continue	
+			logging.info("Restarting stream...")
